cd5b2988c35d5330a956175500bba529703f5a82
daniel sun <daniel.sun@hunterindustries.com>
2018-10-01 20:02:36 +1000

    Update Readme. Homepage.

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { Component } from 'react';
 
-export default class ExpressJS extends React.Component {
-
+export default class Home extends Component {
 	render(){
-		return (	
+		return (
 			<div className="container">
 				<p className="lead">My name is Daniel Sun</p>
-				<p>And I'm a full-stack developer</p>
+				<p>
+					Solid knowledge and experience on web development.
+					I'm mostly experienced in front end development with React line, React+Redux+Antd, and standard front-end tech (HTML5, SASS, Bootstrap, Jest, webpack).
+					My backend experience is mostly lying on PHP, using OO framework to deliver the backend in an efficient and well-tested way.
+					>I have solid experience on two API standards, GraphQL and REST.
+					And I will be more than happy to apply my academic knowledge to introduce machine learning to the products.
+					I'm constantly using linux OS, apache, AWS and Git for work.
+					I enjoyed my careerpath as a web developer as it provides more flexibility compared to native apps.
+					About me?
+					An optimistic and passionate developer always striving to be sharper in the industry.
+					Great willingness to learn and expand beyond my comfort area but extremely cautious when implementing.
+					Strong background with math.
+				</p>
 			</div>
-		)
+		);
 	}
-
 }
