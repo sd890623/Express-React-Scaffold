24bcaf4093e72893506f1c5f977500f9c63388b4
daniel sun <daniel.sun@hunterindustries.com>
2018-10-01 19:02:08 +1000

    Configure dev environment. Clean up code.

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,8 @@
-
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import AppRouter from './router';
-import './index.html';
-
-import 'jquery';
-import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-
-import './style.scss';
 
 ReactDOM.render(
 	<AppRouter />,
