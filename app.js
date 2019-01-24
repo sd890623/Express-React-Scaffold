4fa3509c94eb229b08c3fb3f201d04a4b9a38379
Daniel@Hunter <daniel.sun@hunterindustries.com>
2019-01-24 13:40:07 +1100

    Add loader.

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/api', index);
+app.use('/assets', express.static(path.join(__dirname, 'client/assets')));
 app.use('/', (req, res, next) => res.sendFile(path.join(__dirname, 'client', 'index.html')));
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/server/Views');
